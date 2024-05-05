2ed89cbf430d6e9fb29e1e496d11587d654bd056
Alexey <>
2024-05-05 15:51:18 +0300

    Добавьте файлы проекта.

---
+++ homeworkcalc/Calculate.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeworkcalc
+{
+    public interface ICalc
+    {
+        public double result { get; set; }
+        public void Sum(int x);
+        public void Sub(int x);
+        public void Multy(int x);
+        public void Divide(int x);
+        public void CancelLast();
+        event EventHandler<EventArgs> MyEventHandler;
+    }
+    public class Calculate : ICalc
+    {
+        public double result { get; set; }
+        private Stack<double> lastResult = new Stack<double>();
+
+        public event EventHandler<EventArgs> MyEventHandler;
+
+        public void Divide(int x)
+        {
+            lastResult.Push(result);
+            result /= x;
+            MakeOperation();
+        }
+
+        public void Multy(int x)
+        {
+            lastResult.Push(result);
+            result *= x;
+            MakeOperation();
+        }
+
+        public void Sub(int x)
+        {
+            lastResult.Push(result);
+            result -= x;
+            MakeOperation();
+        }
+
+        public void Sum(int x)
+        {
+            lastResult.Push(result);
+            result += x;
+            MakeOperation();
+        }
+        public void MakeOperation()
+        {
+            PrintResult();
+        }
+        private void PrintResult()
+        {
+            MyEventHandler?.Invoke(this, new EventArgs());
+        }
+        public void CancelLast()
+        {
+            if (lastResult.TryPop(out double res))
+            {
+                result = res;
+                PrintResult();
+            }
+        }
+    }
+}
---
+++ homeworkcalc/Program.cs
@@ -0,0 +1,71 @@
+﻿/* Доработайте программу калькулятор реализовав выбор действий
+ * и вывод результатов на экран в цикле так чтобы калькулятор
+ * мог работать до тех пор пока пользователь не нажмет отмена
+ * или введёт пустую строку. */
+
+using homeworkcalc;
+using System.Runtime.CompilerServices;
+using System.Xml.Linq;
+
+var calc = new Calculate();
+calc.MyEventHandler += Calculate_MyEventHandler;
+calc.MyEventHandler += Congrat_MyEventHandler; ;
+
+
+Console.WriteLine("Введите первое число");
+calc.result = Convert.ToDouble(Console.ReadLine());
+
+while (true)
+{
+
+    Console.WriteLine("Введите действие: '+', '-', '*', '/', 'Отмена действия', 'Выход'");
+    var sight = Console.ReadLine();
+    int secondNumber = 0;
+
+    if (sight != "Отмена" && sight != "Выход")
+    {
+        Console.WriteLine("Введите второе число");
+        secondNumber = Convert.ToInt32(Console.ReadLine());
+    }
+
+    switch (sight)
+    {
+        case "+":
+            calc.Sum(secondNumber);
+            break;
+        case "-":
+            calc.Sub(secondNumber);
+            break;
+        case "/":
+            calc.Divide(secondNumber);
+            break;
+        case "*":
+            calc.Multy(secondNumber);
+            break;
+        case "Отмена":
+            calc.CancelLast();
+            break;
+        case "Выход":
+            Environment.Exit(0);
+            break;
+    }
+
+}
+
+static void Calculate_MyEventHandler(object? sender, EventArgs e)
+{
+    if (sender is Calculate)
+    {
+        Console.WriteLine(String.Empty);
+        Console.WriteLine("Результат:");
+        Console.WriteLine(((Calculate)sender).result);
+    }
+
+}
+
+static void Congrat_MyEventHandler(object? sender, EventArgs e)
+{
+    Console.WriteLine("-----------------------------");
+}
+
+Console.WriteLine(String.Empty);
